a09ab7c5b4cee0d636ab2120173fa473634b7763
Moeez Muhammad <mmoeez48@gmail.com>
2023-12-04 22:41:42 -0500

    Bugfixes on templating

--- build_template.py
+++ build_template.py
@@ -5,15 +5,13 @@ TEMPLATE_PREFIX = "$"
 parentdir = pathlib.Path(__file__).resolve().parent
 templates = {}
 for template_filename in (parentdir / TEMPLATE_DIR).glob('*.html'):
-    print(template_filename)
     with open(template_filename, 'r') as template_file:
         templates[template_filename.name.replace('.html','')] = template_file.read()
 
 for html_filename in parentdir.rglob('*.html'):
-    with open(html_filename, 'w+') as html_file:
+    with open(html_filename, 'r') as html_file:
         s = html_file.read()
         for k, v in templates.items():
             s.replace(TEMPLATE_PREFIX + k, v)
+    with open(html_filename, 'w') as html_file:
         html_file.write(s)
-
-
