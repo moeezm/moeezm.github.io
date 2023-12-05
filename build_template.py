42bf638b35652e909ade8a889b3280cd6f1d1fce
Moeez Muhammad <mmoeez48@gmail.com>
2023-12-05 00:04:35 -0500

    Templating works!

--- build_template.py
+++ build_template.py
@@ -1,17 +1,33 @@
-import pathlib
-TEMPLATE_DIR = pathlib.Path("templates/")
+from pathlib import Path
+from sys import exit
+BUILD_DIR = Path("build/") 
+SRC_DIR = Path("src/")
+TEMPLATE_DIR = Path("templates/")
 TEMPLATE_PREFIX = "$"
 
-parentdir = pathlib.Path(__file__).resolve().parent
-templates = {}
-for template_filename in (parentdir / TEMPLATE_DIR).glob('*.html'):
-    with open(template_filename, 'r') as template_file:
-        templates[template_filename.name.replace('.html','')] = template_file.read()
+FILE_TYPES = ['*.html', '*.css', '*.js']
 
-for html_filename in parentdir.rglob('*.html'):
-    with open(html_filename, 'r') as html_file:
-        s = html_file.read()
+parentdir = Path(__file__).resolve().parent
+templates = {}
+
+def src_to_build(filepath):
+    return parentdir / BUILD_DIR / filepath.relative_to((parentdir / SRC_DIR))
+
+for filetype in FILE_TYPES:
+    for template_filename in (parentdir / TEMPLATE_DIR).glob(filetype):
+        with open(template_filename, 'r') as template_file:
+            templates[template_filename.stem] = template_file.read()
+
+for directory in (parentdir / SRC_DIR).rglob('*'):
+    if (directory.is_dir()):
+        print(directory)
+        src_to_build(directory).mkdir(parents=True, exist_ok=True)
+
+for filetype in FILE_TYPES:
+    for filename in (parentdir / SRC_DIR).rglob(filetype):
+        with open(filename, 'r') as web_file:
+            s = web_file.read()
         for k, v in templates.items():
             s = s.replace(TEMPLATE_PREFIX + k, v)
-    with open(html_filename, 'w') as html_file:
-        html_file.write(s)
+    with open(src_to_build(filename), 'w') as web_file:
+        web_file.write(s)
